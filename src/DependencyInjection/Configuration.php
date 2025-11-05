387427f0985123080b88906bff63fdc8a10fec28
Manuel Kießling <manuel@kiessling.net>
2025-11-05 11:31:44 +0100

    Dependency updates
    - Symfony: 7.* -> 8.0.*
    - Node.js: 22 -> 24
    - NPM dependencies

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,6 +9,9 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
+    /**
+     * @return TreeBuilder<'array'>
+     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('etfs_shared');
